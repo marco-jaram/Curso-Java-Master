81b37e6e854f4d8e60afde5a2fb93bed57fc9cd7
Marco Jaramillo <marco.jaramillocast@gmail.com>
2022-08-08 14:06:43 -0700

    inicio de curso master java

---
+++ HolaMundo/src/SentenciIfElse.java
@@ -0,0 +1,29 @@
+public class SentenciIfElse {
+    public static void main(String[] args) {
+        float promedio = 9.8f;
+        if (promedio >= 6.5) {
+            System.out.println("Bien mi chavo");
+
+        }else {
+            System.out.println("Necesitas echarle mas ganitas mi chavo");
+        }
+
+
+float calis= 5.0f;
+        if (calis  >=6.5){
+            System.out.println("Felicitaciones aprobaste");
+        } else if (calis >= 6.0) {
+            System.out.println("Muy buen promedio");
+        } else if (calis >= 5.5) {
+            System.out.println("andas valiendo");
+        } else if (calis >= 5.0) {
+            System.out.println(" ya valiste tronaste jajaja");
+        } else if (calis >= 4.0) {
+            System.out.println("Ya de plano burro");
+        } else {
+            System.out.println("Tronadote jajaja");
+        }
+        System.out.println("Tu cali es = " + calis);
+    }
+
+}
